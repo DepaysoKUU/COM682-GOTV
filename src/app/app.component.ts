5e4e79d42ef824e523f06c68d5e5574f16887780
DepaysoKUU <depayso-k@ulster.ac.uk>
2024-12-02 16:14:30 +0000

    Adds auto update when adding a new entry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import {HttpClient} from '@angular/common/http';
@@ -19,13 +19,14 @@ export class AppComponent implements OnInit {
   readonly dialog = inject(MatDialog);
 
   GET_ALL_URL = "https://prod-07.uksouth.logic.azure.com/workflows/e5fdd07148c5425fa4b0d297c225fcd4/triggers/When_a_HTTP_request_is_received/paths/invoke/gotv/highlights?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=C5G5uL2_kpLwdEg_-Y4OcAm4X_ua95NIahzaRHvbt1E";
+  GET_SINGLE_URL = "https://prod-15.uksouth.logic.azure.com/workflows/aec09bd28f7e4a9bb1538921d97f53a8/triggers/When_a_HTTP_request_is_received/paths/invoke/gotv/highlight/%7Bid%7D?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=FDtM0B7VTmtzmZ9fGJTwTMruaUOtTfsBkQG9650S-3o"
   BLOB_ACCOUNT = "https://depaysokblob.blob.core.windows.net";
   POST_URL = "https://prod-24.uksouth.logic.azure.com:443/workflows/bae358fbbbd54c2e89764542b2a8e243/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=JHxzsobTTeHaSTWXQjRleJ9gIY15WnMcqMc2-Fzb2ag"
 
   title = 'AngularGOTV';
   highlights: any = [];
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.getHighlights()
@@ -34,21 +35,14 @@ export class AppComponent implements OnInit {
 
   getHighlights(): void {
     this.httpClient.get(this.GET_ALL_URL).subscribe({
-      next: data => {
-        this.highlights.push(data);
-        console.log(data);
+      next: (data:any) => {
+        this.highlights = [...this.highlights, ...data];
+        this.cdr.detectChanges();
       },
       error: err => {
         console.log(err);
       }
     })
-  }
-
-  getFileFromBlob(filePath: string): any {
-    let bloburl = this.BLOB_ACCOUNT + filePath;
-    console.log(bloburl);
-
-    return this.BLOB_ACCOUNT + filePath;
   }
 
   openAddHighlightDialog() {
@@ -85,10 +79,22 @@ export class AppComponent implements OnInit {
           const fileUrl = `https://${account}.blob.core.windows.net/${containerName}/${blobName}`;
           postData.set('fileUrl', fileUrl);
           this.httpClient.post<any>(this.POST_URL, postData).subscribe({
-            next: data => {console.log("Uploaded FIle")}
+            next: (data: any) => {
+              let get_url = this.GET_SINGLE_URL.replace("%7Bid%7D", blobName)
+
+              this.httpClient.get(get_url).subscribe({
+                next: (data:any) => {
+                  console.log(data)
+                  this.highlights = [...this.highlights, data["Documents"][0]];
+                  this.cdr.detectChanges();
+                },
+                error: err => {
+                  console.log(err);
+                }
+              })
+            }
           })
         })
     });
   }
-
 }
